8478251314638a3c55a6b83446ca9545c0ab87ca
Newton Pohsner <npohsner@gmail.com>
2023-10-31 12:58:01 -0300

    Alteração de nome

--- Configurações_Essenciais.py
+++ Configurações_Essenciais.py
@@ -8,18 +8,20 @@ arguments = ["--lang=pt-BR","--window-size=800,800","--incognito"]
 for argument in arguments:
     chrome_options.add_argument(argument)
 # Uso de conficurações Experimentais
-chrome_options.add_experimental_option("prefs",{
-    # Alterar local padrão do arquivo
-    "download.default.directory":"C:\\Users\\Nextpc\\Desktop\\CURSO MESTRE DA AUTOMAÇÃO\\projetos-selenium\\downloads"
-    # Notificar o google chrome sobre ess alteração
-    "download.directory_upgrade": ,
-    # Desabilitar a confirmação de download
-    "download.prompt_for_dowload": False,
-    # Desabilitar notificações do navegador
-    "profile.default._content_setting_values.notifications": 2,
+chrome_options.add_experimental_option('prefs', {
+    # Alterar local padrão de download de arquivos
+    'dowload.default_directory': 'C:\\Users\\Nextpc\\Desktop\\CURSO MESTRE DA AUTOMAÇÃO\\projetos-selenium\\downloads',
+    # Notificar google chrome sobre a alteração
+    'download.directory_upgrade': True,
+    # Desabilitar confirmação de Download
+    'download.prompt_for_download': False,
+    # Desabilitar notificações
+    'profile.default_content_setting_values.notifications': 2,
     # Permitir multiplos downloads
-    "profile.default._content_setting_values.automatic_downloads": 1,
-}) 
+    'profile.default_content_setting_values.automatic_downloads': 1,
+})
+
+ 
 # Inicializando Webdriver
 driver = webdriver.Chrome(service=chromeService(ChromeDriverManager().install()),options=chrome_options)
 # Navegando até um site
