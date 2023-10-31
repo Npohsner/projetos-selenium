ab1f41d2e3ff36e86a489b594a3836627862867b
Newton Pohsner <npohsner@gmail.com>
2023-10-31 02:54:08 -0300

    projetos-selenium

---
+++ app.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as chromeService
+from webdriver_manager.chrome import ChromeDriverManager
+# Inicializando Webdriver
+driver = webdriver.Chrome(service=chromeService(ChromeDriverManager().install()))
+# Navegando até um site
+driver.get("https://youtube.com")
+# Necessario fazer algo, caso contrario ele executa e fecha
+input("Aperte uma tecla para fechar;")
